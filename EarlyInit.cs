9960c2e3d778ecdc47e488654d8cd9ea5816275d
LostIntelligence <usb4@web.de>
2025-12-07 18:37:59 +0100

    Forgot to load user settings during Init

--- EarlyInit.cs
+++ EarlyInit.cs
@@ -12,20 +12,25 @@ namespace HappyDiggingDwarfEditionFixed
 
         private static HappyDiggingSettings settings;
         public static HappyDiggingSettings Settings => settings;
+        
         public override void OnLoad(Harmony harmony)
         {
             PUtil.InitLibrary(false);
 
-            // Initialize new PLib settings
-            settings = new HappyDiggingSettings();
-         
-
-            // Register PLib options
+            // Register options with PLib
             new POptions().RegisterOptions(this, typeof(HappyDiggingSettings));
+
+            // Load saved user settings from disk
+            var settings = POptions.ReadSettings<HappyDiggingSettings>();
+            if (settings == null)
+                settings = new HappyDiggingSettings();
+            // Send Settings to config
             Config.Init(settings);
+
             base.OnLoad(harmony);
             EarlyInit.PostInit();
         }
+
 
         public static void PostInit()
         {
